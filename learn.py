f8da27322a7b7649e52621ae06c7e597feb92a5d
Suraj Trimukhe <suraj.trimukhe@punesez.searce.local>
2020-08-10 15:50:34 +0530

    firstcommit

---
+++ learn.py
@@ -0,0 +1,43 @@
+print ("hello")
+if 5>3:
+ print("five is great")
+ print("sdnf")
+ x = 5
+ y = "hello, hi"
+ x = 5
+ y = "Hello, World!"
+
+ print(x)
+ print(y)
+
+
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+x, y, z = "ora", "ban", "che"
+print(x)
+print(y)
+print(z)
+
+
+x = "awesome"
+print("python is " + x)
+
+x = "Python is "
+y = "awesome"
+z =  x + y
+print(z)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+x = b"Hello"
+
+
+
+#display the data type of x:
+print(type(x)) 
